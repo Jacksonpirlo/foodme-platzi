cefd18f6450f085684c9a76532df8aceaf660bf5
Jacksonpirlo <jacksonflorezp@gmail.com>
2024-06-20 19:04:06 -0500

    Otros cambios en newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -1,11 +1,12 @@
 'use strict'
 exports.config={ 
     app_name:['Foodme'],
-    license_key:process.env.LICENSE_KEY,
+    license_key:process.env.LICENSE_KEY || "2a717e2292bbbe5558951d66aa05703972a7NRAL",
     logging:{level:'info'},
     allow_all_headers:true,
     attributes:{
-        exclude:['request.headers.cookie',
+        exclude:[
+        'request.headers.cookie',
         'request.headers.authorization',
         'request.headers.proxyAuthorization',
         'request.headers.setCookie*',
